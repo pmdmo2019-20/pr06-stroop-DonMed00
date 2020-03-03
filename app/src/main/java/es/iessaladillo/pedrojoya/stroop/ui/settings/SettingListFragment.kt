697b155895f686d0b67a7c4ec225be520845fc73
DonMeD <xmena97@gmail.com>
2020-03-03 23:05:50 +0100

    he borrao settings sin querer por eso este commit

---
+++ app/src/main/java/es/iessaladillo/pedrojoya/stroop/ui/settings/SettingListFragment.kt
@@ -0,0 +1,11 @@
+package es.iessaladillo.pedrojoya.stroop.ui.settings
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import es.iessaladillo.pedrojoya.stroop.R
+
+class SettingListFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences,rootKey)
+    }
+}
